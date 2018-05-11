4517c67c70ccf98c540e0b5b13dd23d6d77258a9
Kurtis Houser <houserkurtis@gmail.com>
2018-05-11 12:20:02 -0700

    Update MapAsset model with timestamps and collection name

--- server/boot/create-sample-models.js
+++ server/boot/create-sample-models.js
@@ -9,6 +9,8 @@ module.exports = function(app) {
       state: 'CA',
       postalCode: '94117',
       geoJson: '{ "type": "Feature", "properties": {}, "geometry": { "type": "Point", "coordinates": [ -122.43470907211304, 37.7763839383027 ] } }',
+      createdAt: Date.now(),
+      updatedAt: Date.now(),
     }, {
       name: 'St Mary\'s Medical Center',
       addressLine1: '450 Stanyan St',
@@ -16,6 +18,8 @@ module.exports = function(app) {
       state: 'CA',
       postalCode: '94117',
       geoJson: '{ "type": "Feature", "properties": {}, "geometry": { "type": "Point", "coordinates": [ -122.45367228984833, 37.77402853821361 ] } }',
+      createdAt: Date.now(),
+      updatedAt: Date.now(),
     }, {
       name: 'Chevron',
       description: 'Corner of Fell St & Masonic Ave',
@@ -24,6 +28,8 @@ module.exports = function(app) {
       state: 'CA',
       postalCode: '94117',
       geoJson: '{ "type": "Feature", "properties": {}, "geometry": { "type": "Point", "coordinates": [ -122.44560956954957, 37.77321494311367 ] } }',
+      createdAt: Date.now(),
+      updatedAt: Date.now(),
     }], function(err, mapAssets) {
       if (err) throw err;
 
